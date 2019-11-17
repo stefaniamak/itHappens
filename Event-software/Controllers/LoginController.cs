4f2fe1d346aac6e17f8246fd9fbe0082009f1b0a
KonstantinaK98 <konakara98@gmail.com>
2019-11-17 21:57:57 +0200

    Event-software

---
+++ Event-software/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itHappens.Controllers
+{
+    class LoginController
+    {
+
+        public static void FieldsValidation(String username,String password)
+        {
+            if (String.IsNullOrEmpty(username) &&
+                String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Fill the fields!", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (String.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Invalid password!", "Warning",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                
+                
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Event-software/UIs/LoginForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace itHappens
+{
+    public partial class LogEvents : Form
+    {
+        public LogEvents()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }       
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+            itHappens.Controllers.LoginController.FieldsValidation(UsernameTextBox.Text,PasswordTextBox.Text);
+            UsernameTextBox.Text = "";
+            PasswordTextBox.Text = "";
+        }
+
+       
+
+        
+
+        private void GooglePictureBox_Click(object sender, EventArgs e)
+        {
+            var GForm = new GoogleForm();
+            GForm.Show();
+        }
+
+        private void PasswordTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals(Convert.ToChar(13)))
+            {
+                LoginButton_Click(sender, e);
+            }
+        }
+    }
+}
