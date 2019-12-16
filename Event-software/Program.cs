cfb7069ef385c1251aee0a36a1c3c6e6c787015e
KonstantinaK98 <konakara98@gmail.com>
2019-12-16 23:12:47 +0200

    Change Program.cs(SampleForm)

--- Event-software/Program.cs
+++ Event-software/Program.cs
@@ -18,7 +18,7 @@ namespace itHappens
             Db_connector Alpha = new Db_connector();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LogEvents());
+            Application.Run(new UIs.SampleForm());
         }
     }
 }
