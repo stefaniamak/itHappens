dfe2618198932289a20877914670fbcbd3487cc8
Stoy-atd <57042447+Stoy-atd@users.noreply.github.com>
2019-12-21 19:29:16 +0200

    api try

--- Event-software/Program.cs
+++ Event-software/Program.cs
@@ -19,6 +19,7 @@ namespace itHappens
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LogEvents());
+
         }
     }
 }
