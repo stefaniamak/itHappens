12f8253f87770fe78365bf0a579a85838d09b0a3
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2019-12-29 13:36:21 +0200

    Test

--- Event-software/UIs/valentina/ListsContentPage.Designer.cs
+++ Event-software/UIs/valentina/ListsContentPage.Designer.cs
@@ -61,7 +61,7 @@
             this.AutoScroll = true;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackColor = System.Drawing.Color.Thistle;
             this.Controls.Add(this.ListsContentTitle);
             this.Controls.Add(this.EventHolderFlowLayoutPanel);
             this.Name = "ListsContentPage";
