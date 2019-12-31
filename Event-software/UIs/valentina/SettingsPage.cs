f9ef21ecf6ed8bb1d0c39ffd09a81cac54083cf9
KonstantinaK98 <konakara98@gmail.com>
2019-12-31 15:05:39 +0200

    Delete of some unused methods on SettingsPage

--- Event-software/UIs/valentina/SettingsPage.cs
+++ Event-software/UIs/valentina/SettingsPage.cs
@@ -17,24 +17,5 @@ namespace itHappens.UIs.valentina
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label6_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
