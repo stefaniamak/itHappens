c7759d6d18447c2bd43f36f57c3460dcd85faf42
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2020-01-04 14:16:00 +0200

    Small corractions

--- Event-software/UIs/Main/CommonSearchTextPage.Designer.cs
+++ Event-software/UIs/Main/CommonSearchTextPage.Designer.cs
@@ -31,7 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CommonSearchTextPage));
             this.usersLocationLabel = new System.Windows.Forms.Label();
             this.searchTextBox = new System.Windows.Forms.TextBox();
-            this.hostPanel = new System.Windows.Forms.Panel();
+            hostPanel = new System.Windows.Forms.Panel();
             this.searchPanel = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -45,9 +45,10 @@
             this.usersLocationLabel.AutoSize = true;
             this.usersLocationLabel.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.usersLocationLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.usersLocationLabel.Location = new System.Drawing.Point(37, 12);
+            this.usersLocationLabel.Location = new System.Drawing.Point(56, 37);
+            this.usersLocationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.usersLocationLabel.Name = "usersLocationLabel";
-            this.usersLocationLabel.Size = new System.Drawing.Size(118, 20);
+            this.usersLocationLabel.Size = new System.Drawing.Size(168, 25);
             this.usersLocationLabel.TabIndex = 4;
             this.usersLocationLabel.Text = "User\'s Location";
             // 
@@ -56,31 +57,34 @@
             this.searchTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.searchTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.searchTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.searchTextBox.Location = new System.Drawing.Point(32, 3);
+            this.searchTextBox.Location = new System.Drawing.Point(49, 5);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.searchTextBox.Name = "searchTextBox";
-            this.searchTextBox.Size = new System.Drawing.Size(322, 21);
+            this.searchTextBox.Size = new System.Drawing.Size(483, 28);
             this.searchTextBox.TabIndex = 5;
             this.searchTextBox.Text = "Want to go out?";
             // 
             // hostPanel
             // 
-            this.hostPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            hostPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.hostPanel.AutoScroll = true;
-            this.hostPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.hostPanel.Location = new System.Drawing.Point(0, 108);
-            this.hostPanel.Name = "hostPanel";
-            this.hostPanel.Size = new System.Drawing.Size(468, 309);
-            this.hostPanel.TabIndex = 6;
+            hostPanel.AutoScroll = true;
+            hostPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            hostPanel.Location = new System.Drawing.Point(0, 166);
+            hostPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            hostPanel.Name = "hostPanel";
+            hostPanel.Size = new System.Drawing.Size(702, 475);
+            hostPanel.TabIndex = 6;
             // 
             // searchPanel
             // 
             this.searchPanel.BackColor = System.Drawing.Color.White;
             this.searchPanel.Controls.Add(this.tableLayoutPanel1);
-            this.searchPanel.Location = new System.Drawing.Point(38, 35);
+            this.searchPanel.Location = new System.Drawing.Point(57, 73);
+            this.searchPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.searchPanel.Name = "searchPanel";
-            this.searchPanel.Size = new System.Drawing.Size(357, 26);
+            this.searchPanel.Size = new System.Drawing.Size(536, 40);
             this.searchPanel.TabIndex = 7;
             // 
             // tableLayoutPanel1
@@ -92,10 +96,11 @@
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(357, 26);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(536, 40);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -103,22 +108,24 @@
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(23, 20);
+            this.pictureBox1.Size = new System.Drawing.Size(37, 30);
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
             // CommonSearchTextPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.Controls.Add(this.searchPanel);
-            this.Controls.Add(this.hostPanel);
+            this.Controls.Add(hostPanel);
             this.Controls.Add(this.usersLocationLabel);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "CommonSearchTextPage";
-            this.Size = new System.Drawing.Size(468, 417);
+            this.Size = new System.Drawing.Size(702, 642);
             this.searchPanel.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
@@ -135,6 +142,6 @@
         private System.Windows.Forms.Panel searchPanel;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        public System.Windows.Forms.Panel hostPanel;
+        public static System.Windows.Forms.Panel hostPanel;
     }
 }
