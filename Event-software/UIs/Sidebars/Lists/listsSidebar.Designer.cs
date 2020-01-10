ff3b713837f7a0d457b97a9087780ed92dfb4b5e
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2020-01-10 23:59:52 +0200

    Create Event User Control

--- Event-software/UIs/Sidebars/Lists/listsSidebar.Designer.cs
+++ Event-software/UIs/Sidebars/Lists/listsSidebar.Designer.cs
@@ -32,7 +32,8 @@
             this.listsPanel = new System.Windows.Forms.Panel();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.listsFlowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.createListButton = new System.Windows.Forms.Button();
+            this.createListPage = new itHappens.UIs.Sidebars.Lists.CreateListPage();
             this.listsPanel.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
@@ -67,8 +68,9 @@
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.createListPage);
+            this.flowLayoutPanel1.Controls.Add(this.createListButton);
             this.flowLayoutPanel1.Controls.Add(this.listsFlowLayoutPanel);
-            this.flowLayoutPanel1.Controls.Add(this.button1);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 0);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -80,22 +82,33 @@
             // 
             this.listsFlowLayoutPanel.AutoSize = true;
             this.listsFlowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.listsFlowLayoutPanel.Location = new System.Drawing.Point(3, 3);
+            this.listsFlowLayoutPanel.Location = new System.Drawing.Point(3, 128);
             this.listsFlowLayoutPanel.Name = "listsFlowLayoutPanel";
             this.listsFlowLayoutPanel.Size = new System.Drawing.Size(0, 0);
             this.listsFlowLayoutPanel.TabIndex = 0;
             // 
-            // button1
+            // createListButton
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
-            this.button1.Location = new System.Drawing.Point(3, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "+ CREATE LIST";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            this.createListButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.createListButton.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.createListButton.Location = new System.Drawing.Point(3, 99);
+            this.createListButton.Name = "createListButton";
+            this.createListButton.Size = new System.Drawing.Size(90, 23);
+            this.createListButton.TabIndex = 1;
+            this.createListButton.Text = "+ CREATE LIST";
+            this.createListButton.UseVisualStyleBackColor = false;
+            this.createListButton.Click += new System.EventHandler(this.button1_Click_1);
+            this.createListButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // createListPage
+            // 
+            this.createListPage.BackColor = System.Drawing.Color.LightGray;
+            this.createListPage.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createListPage.Location = new System.Drawing.Point(3, 4);
+            this.createListPage.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.createListPage.Name = "createListPage";
+            this.createListPage.Size = new System.Drawing.Size(136, 88);
+            this.createListPage.TabIndex = 2;
             // 
             // ListsSidebar
             // 
@@ -123,6 +136,7 @@
         private System.Windows.Forms.Panel listsPanel;
         private System.Windows.Forms.FlowLayoutPanel listsFlowLayoutPanel;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button createListButton;
+        private Lists.CreateListPage createListPage;
     }
 }
--- Event-software/UIs/Sidebars/Lists/listsSidebar.cs
+++ Event-software/UIs/Sidebars/Lists/listsSidebar.cs
@@ -22,9 +22,7 @@ namespace itHappens.UIs.Sidebars
         {
             InitializeComponent();
 
-            //listsFlowLayoutPanel.Controls.Add(new Lists.ListsMini("hey"));
-
-
+            createListPage.Visible = false;
             fillAllLists();
             
         }
@@ -73,12 +71,17 @@ namespace itHappens.UIs.Sidebars
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button1_MouseClick(object sender, MouseEventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            createListPage.Visible = !createListPage.Visible;
         }
     }
 }
