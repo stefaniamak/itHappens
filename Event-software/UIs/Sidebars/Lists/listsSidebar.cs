1a3a3a626bb27aaa6512ae2786f7cccb786831a3
Stoy-atd <57042447+Stoy-atd@users.noreply.github.com>
2020-01-10 19:18:19 +0200

    Enfanisi dedomenon sidebar oxi olokriromeno

--- Event-software/UIs/Sidebars/Lists/listsSidebar.cs
+++ Event-software/UIs/Sidebars/Lists/listsSidebar.cs
@@ -7,28 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using dbstuff;
 
 namespace itHappens.UIs.Sidebars
 {
     public partial class ListsSidebar : UserControl
     {
+        private static DbConnector dbCon = new DbConnector();
+        private static string conStr = dbCon.GetConnectionString();
         public ListsSidebar()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 10; i++)
+            MySqlConnection con;
+            MySqlCommand command;
+            MySqlDataReader dataReader;
+            try
             {
-                listsFlowLayoutPanel.Controls.Add(new Lists.ListsMini());
-                //listsHorizontalListing.SetFlowBreak(listsHorizontalListing, true);
+                con = new MySqlConnection(conStr);
+                con.Open();
+                string queryString = "SELECT (*) FROM event_list";
+
+                command = new MySqlCommand(queryString, con);
+                dataReader = command.ExecuteReader();
+                int List = (Convert.ToInt32(dataReader.GetString(0)));
+                while (dataReader.Read())
+                {
+                    for (int i = 0; i < List; i++)
+                    {
+                        listsFlowLayoutPanel.Controls.Add(new Lists.ListsMini(dataReader.GetString(2)));
+
+                    }
+                    con.Close();
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error");
             }
         }
 
+
         private void listsHorizontalListing_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void carousel1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
